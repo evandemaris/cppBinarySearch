2411ceab812fa8a32371120db9c04ed23c248c96
Evan Demaris <evandemaris@gmail.com>
2020-04-21 09:49:52 -0700

    Update to add skeleton code

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
 //Algorithm for performing a binary search through a sorted array.
 //Binary search should allow significantly lower seek times to find data.
 
+int BinarySearch(int myArray[], int arrayLength, int target) {
+	return 0;
+}
