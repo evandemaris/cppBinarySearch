266d220f54e76911e4be2c1dfa1cf813b7da3b11
Evan Demaris <evandemaris@gmail.com>
2020-04-23 08:53:29 -0700

    Updated with instructions

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,6 +1,14 @@
 //Algorithm for performing a binary search through a sorted array.
-//Binary search should allow significantly lower seek times to find data.
+//Binary search should provide O(logn) performance.
 
+using namespace std;
+
+//Function will require the integer array to search through, the length of the array, and the target value to search for
 int BinarySearch(int myArray[], int arrayLength, int target) {
+	int left = 0; //Smallest value to search through, initialized to start of array
+	int right = arrayLength-1; //Largest value to search through, initialized to end of array
+
+	//Loop:
+
 	return 0;
 }
