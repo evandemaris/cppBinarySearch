3886a9daffe7d9f9ff6635a1f32713f2277ee659
Evan Demaris <evandemaris@gmail.com>
2020-04-25 20:32:54 -0700

    Updated with recursive loop

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -7,8 +7,10 @@ using namespace std;
 int BinarySearch(int myArray[], int arrayLength, int target) {
 	int left = 0; //Smallest value to search through, initialized to start of array
 	int right = arrayLength-1; //Largest value to search through, initialized to end of array
-
+	int midpoint;
 	//Loop:
-
-	return 0;
+	if (right > 0) { //If there is more than one element
+		midpoint = right / 2 + 1; //Assign the midpoint of the elements as the last index divided by two. Integer division may place this too low, as in 1/2=0, so add 1.
+	}
+	return -1; //target is not in array.
 }
