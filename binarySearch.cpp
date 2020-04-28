8f9460f68513767ff4c1a0c88b69839606faa02e
Evan Demaris <evandemaris@gmail.com>
2020-04-27 21:50:31 -0700

    Correction to recursion

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -3,14 +3,14 @@
 
 using namespace std;
 
-//Function will require the integer array to search through, the length of the array, and the target value to search for
-int BinarySearch(int myArray[], int arrayLength, int target) {
-	int left = 0; //Smallest value to search through, initialized to start of array
-	int right = arrayLength-1; //Largest value to search through, initialized to end of array
+//Function will require the integer array to search through, the length of the array, and the target value to search for. Optionally take a start value.
+int BinarySearch(int myArray[], int arrayLength, int target, int left=0) {
 	int midpoint;
 	//Loop:
-	if (right > 0) { //If there is more than one element
-		midpoint = right / 2 + 1; //Assign the midpoint of the elements as the last index divided by two. Integer division may place this too low, as in 1/2=0, so add 1.
+	if (arrayLength > 1) { //If there is more than one element
+		midpoint = arrayLength / 2 + 1; //Assign the midpoint of the elements as the last index divided by two. Integer division may place this too low, as in 1/2=0, so add 1.
+		if (myArray[midpoint] == target) { return midpoint; }
+		if (myArray[midpoint] < target) { BinarySearch(myArray, arrayLength, target, midpoint); }
 	}
 	return -1; //target is not in array.
 }
