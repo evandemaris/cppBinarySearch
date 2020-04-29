7ec0f355dae612ecd8bd10be39ebc8794d1ba9a0
Evan Demaris <evandemaris@gmail.com>
2020-04-28 22:54:48 -0700

    adding third if

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -11,6 +11,7 @@ int BinarySearch(int myArray[], int arrayLength, int target, int left=0) {
 		midpoint = arrayLength / 2 + 1; //Assign the midpoint of the elements as the last index divided by two. Integer division may place this too low, as in 1/2=0, so add 1.
 		if (myArray[midpoint] == target) { return midpoint; }
 		if (myArray[midpoint] < target) { BinarySearch(myArray, arrayLength, target, midpoint); }
+		if (myArray[midpoint] > target) { BinarySearch(myArray, arrayLength/2+1, target, left); }
 	}
 	return -1; //target is not in array.
 }
