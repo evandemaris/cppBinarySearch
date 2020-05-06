4cd3372571e6c099b84a9920d59e54fc5f9bd882
Evan Demaris <evandemaris@gmail.com>
2020-05-05 23:24:00 -0700

    fixme added for recursion error

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -10,9 +10,9 @@ int BinarySearch(int myArray[], int arrayLength, int target, int left=0) {
 	if (arrayLength > 1) { //If there is more than one element
 		midpoint = (arrayLength - 1 ) / 2 + 1;
 
-		cout << (myArray[midpoint] == target) << endl; //testing
+		cout << midpoint << endl; //testing
 		if (myArray[midpoint] == target) { return midpoint; }
-		if (myArray[midpoint] < target) { return BinarySearch(myArray, arrayLength, target, midpoint); }
+		if (myArray[midpoint] < target) { return BinarySearch(myArray, arrayLength, target, midpoint); } //FIXME: midpoint for the recursive algorithm is using the full arraylength instead of [left..arrayLength]/2
 		if (myArray[midpoint] > target) { return BinarySearch(myArray, arrayLength/2+1, target, left); }
 	}
 	return -1; //target is not in array.
@@ -22,7 +22,7 @@ int BinarySearch(int myArray[], int arrayLength, int target, int left=0) {
 //testing
 int main() {
 	int testArray[] = { 1,2,3,4,5 }; 
-	int targ = 4; //works on 2,3,4. fails on 1,5
+	int targ = 5; //works on 2,3,4. fails on 1,5
 	cout << BinarySearch(testArray, sizeof(testArray) / sizeof(testArray[0]), targ); //sizeof used to find array length
 }
 
