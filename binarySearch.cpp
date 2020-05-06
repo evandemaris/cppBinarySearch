e022cc9ec1683ac51a0767af97fa82e38712426d
Evan Demaris <evandemaris@gmail.com>
2020-05-06 09:39:24 -0700

    fixed recursion error, now works for all values in array, but fails on value higher than array maximum

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -4,16 +4,15 @@
 using namespace std;
 
 //Function will require the integer array to search through, the length of the array, and the target value to search for. Optionally take a start value.
-int BinarySearch(int myArray[], int arrayLength, int target, int left=0) {
-	int midpoint;
+int BinarySearch(int myArray[], int left, int right, int target) {
 	//Loop:
-	if (arrayLength > 1) { //If there is more than one element
-		midpoint = (arrayLength - 1 ) / 2 + 1;
+	if (right >= 1) { //If there is more than one element
+		int midpoint = (right - left ) / 2 + left; // midpoint is half of the given array length (right minus left), starting at the given leftmost value
 
 		cout << midpoint << endl; //testing
 		if (myArray[midpoint] == target) { return midpoint; }
-		if (myArray[midpoint] < target) { return BinarySearch(myArray, arrayLength, target, midpoint); } //FIXME: midpoint for the recursive algorithm is using the full arraylength instead of [left..arrayLength]/2
-		if (myArray[midpoint] > target) { return BinarySearch(myArray, arrayLength/2+1, target, left); }
+		if (myArray[midpoint] < target) { return BinarySearch(myArray, midpoint, right, target); }
+		if (myArray[midpoint] > target) { return BinarySearch(myArray, left, midpoint, target); }
 	}
 	return -1; //target is not in array.
 }
@@ -22,7 +21,7 @@ int BinarySearch(int myArray[], int arrayLength, int target, int left=0) {
 //testing
 int main() {
 	int testArray[] = { 1,2,3,4,5 }; 
-	int targ = 5; //works on 2,3,4. fails on 1,5
-	cout << BinarySearch(testArray, sizeof(testArray) / sizeof(testArray[0]), targ); //sizeof used to find array length
+	int targ = 16; //fails for target too high
+	cout << BinarySearch(testArray, 0, sizeof(testArray) / sizeof(testArray[0]), targ); //sizeof used to find array length - size of full array divided by size of each index
 }
 
