f5fa6f404c90427d5927e71e7d6695b3cbe8c639
Evan Demaris <evandemaris@gmail.com>
2020-05-08 17:33:34 -0700

    added if statement for value not found in array

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -5,23 +5,20 @@ using namespace std;
 
 //Function will require the integer array to search through, the length of the array, and the target value to search for. Optionally take a start value.
 int BinarySearch(int myArray[], int left, int right, int target) {
-	//Loop:
-	if (right >= 1) { //If there is more than one element
+	if (right >= 1) { //If there is at least one element
 		int midpoint = (right - left ) / 2 + left; // midpoint is half of the given array length (right minus left), starting at the given leftmost value
-
-		cout << midpoint << endl; //testing
 		if (myArray[midpoint] == target) { return midpoint; }
+		if (midpoint == left || midpoint == right) { return -1; }
 		if (myArray[midpoint] < target) { return BinarySearch(myArray, midpoint, right, target); }
 		if (myArray[midpoint] > target) { return BinarySearch(myArray, left, midpoint, target); }
 	}
-	return -1; //target is not in array.
 }
 
 
 //testing
 int main() {
 	int testArray[] = { 1,2,3,4,5 }; 
-	int targ = 16; //fails for target too high
+	int targ = 0;
 	cout << BinarySearch(testArray, 0, sizeof(testArray) / sizeof(testArray[0]), targ); //sizeof used to find array length - size of full array divided by size of each index
 }
 
